5b309c1f16aa27ff3c590c00a7ca65c1c12ed05e
HandsomeWeeb5 <farhantb2000@gmail.com>
2022-06-07 15:18:01 +0700

    Update 17: Checkpoint 2

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
+const { readFileSync } = require('fs'); 
 const ejs = require('ejs'); //? INFO (ejs = "Embedded JavaScript templates")
 
 
@@ -14,15 +16,18 @@ app.use(express.urlencoded(
 //* Parsing (uraikan) data
 app.use(express.json())
 
-//* Set view Engine EJS
-app.set("view engine", 'ejs'); 
-
 //* buat Static files
-//app.use(express.static('./public'))
+app.use(express.static(__dirname + 'public'))
 
 /* ======== RENDER PAGES WITH URL ======== */
-app.get('/', (res, req) => {
-    
+app.get('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, '/client/public/index.html'))
+    res.writeHead(200, { "content-type" : "text/html" })
+    res.write(readFileSync('client/index.html'))
+    res.end()
 })
 
 /*======== MULTIPLE PAGE ROUTER ==========*/
+app.listen(7000, () => {
+    console.log('listening to server, http://localhost:7000')
+})
