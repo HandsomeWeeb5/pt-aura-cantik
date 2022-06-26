e70f2b3ecef8f91691e99fb531fb77d4cedc915c
HandsomeWeeb5 <farhantb2000@gmail.com>
2022-06-26 16:02:08 +0700

    Update 50

--- server/controllers/pemasukan.controller.js
+++ server/controllers/pemasukan.controller.js
@@ -18,10 +18,26 @@ const viewBarang = async(req, res) => {
 // Pagination
 const viewBarangPerPage = async(req, res) => {
     let { page, dataPerPage } = req.query;
+    page ? Number(page) : 1;
     dataPerPage = 5;
     const results = await barangService.viewBarangPerPage(page, 5);
+    const numberOfPages = Math.ceil(results.length / dataPerPage);
+
+    
+    if(page > numberOfPages){
+        res.redirect('/?page=' + encodeURIComponent(numberOfPages));
+    } else if (page < 1){
+        res.redirect('/?page=' + encodeURIComponent('1'));
+    }
+
     try{
-        res.json(results);
+        // res.json(results);
+        let iterator = (page - 5) < 1 ? 1 : page - 5;
+        let endingLink = (iterator + 9) <= numberOfPages ? (iterator + 9) : page + (numberOfPages - page);
+        if(endingLink < (page + 4)){
+            iterator -= (page + 4) - numberOfPages;
+        }
+        res.render('pemasukan', {data: results, page, iterator, endingLink, numberOfPages});
     }
     catch (err) {
         console.error('Kesalahan Penampilan Barang: ', err.message);
